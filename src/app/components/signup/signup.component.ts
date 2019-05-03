b1eb6ba666e4c093f182b40d2ce91e1399f6110e
juhavuo <juha.vuokko@pp.inet.fi>
2019-05-03 16:47:17 +0300

    add prechecks to passport and user before sending to server

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { BackendConnectorService } from '../../services/backend-connector.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -13,19 +14,71 @@ export class SignupComponent implements OnInit {
     username: '',
     password: ''
   };
+  password_second: string = '';
+  error_message: string = '';
+  show_error_div: boolean = false;
 
 
-  constructor(private backendConnectorService: BackendConnectorService) {
+  constructor(private backendConnectorService: BackendConnectorService, private router: Router) {
     console.log(this.backendConnectorService);
   }
 
   ngOnInit() {
   }
 
-  register(){
-    this.backendConnectorService.register(this.user).subscribe(response =>{
-      console.log(response);
-    });
+  register() {
+    if(this.user.username.length == 0){
+      this.display_error_message('Enter username');
+    }
+
+    if (this.password_second == this.user.password) {
+      if (this.qualifyPassword(this.user.password)) {
+        
+        this.backendConnectorService.register(this.user).subscribe(response => {
+          console.log(response);
+          this.router.navigate(['login']);
+        });
+      }
+    } else {
+      this.display_error_message('password and password retype don\'t match');
+    }
+  }
+
+  public hideErrors() {
+    this.show_error_div = false;
+    this.error_message = '';
+  }
+
+  private qualifyPassword(pword: string) {
+    this.hasNumembersAndLetters(pword);
+    if (pword.length < 8) {
+      this.display_error_message('password must be 8 characters long');
+      return false;
+    }else if(!this.hasNumembersAndLetters(pword)){
+      this.display_error_message('password must have both numbers and letters');
+      return false;
+    }else {
+      return true;
+    }
+  }
+
+  private display_error_message(em: string){
+    this.show_error_div = true;
+    this.error_message = em;
+  }
+
+  private hasNumembersAndLetters(em: string){
+    const numberPattern = /[0-9]/g;
+    const letterPattern = /[A-Z|a-z|ä|Ä|ö|Ö|å|Å]/g;
+
+    const matchesNumbers = em.match(numberPattern);
+    const matchesLetters = em.match(letterPattern);
+
+    if(matchesNumbers != null && matchesLetters != null){
+      return true;
+    }else{
+      return false;
+    }
   }
 
 }
